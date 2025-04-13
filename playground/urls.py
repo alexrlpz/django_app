53763c6bee7effeb9262c640b905a1b21335dc8a
Alejandro Rueda López <mralejandrorueda@gmail.com>
2025-04-13 13:45:05 +0200

    Include some URLs

---
+++ playground/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.greeting_function,name="index"),
+    path('about',views.about_function,name="about"),
+    path('add/<int:a>/<int:b>',views.add_function,name="add"),
+    path('intro/<str:name>/<int:age>',views.intro_function,name="intro")
+]
--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
 
 # Create your views here.
+
+def greeting_function(request):
+    return HttpResponse("Hello!")
+
+def about_function(request):
+    return HttpResponse("This is a test project using Django")
+
+def add_function(request,a,b):   #must be 'a' and 'b' because we defined like this on urls.py
+    return HttpResponse(a+b)
+
+def intro_function(request,name,age):
+    my_dictionary = {
+        "name" : name,
+        "age" : age
+    }
+    return JsonResponse(my_dictionary)
